b416c7713f7071f6ac47cfe5f4a83763a66c76bf
ffshyw1-star <ffshyw1@gmail.com>
2026-07-27 00:27:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
+
+const db = require("./server/database/database");
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server);
 
+app.use(express.json());
 app.use(express.static("public"));
+
+let onlineUsers = {};
 
 io.on("connection", (socket) => {
 
-    console.log("User Connected");
+    console.log("User Connected:", socket.id);
+
+    socket.on("join-user", (user) => {
+
+        onlineUsers[socket.id] = user;
+
+        console.log("New User:", user.username);
+
+        io.emit("users-online", Object.values(onlineUsers));
+
+    });
+
 
     socket.on("chat-message", (data) => {
 
@@ -19,16 +36,22 @@ io.on("connection", (socket) => {
 
     });
 
+
     socket.on("disconnect", () => {
 
-        console.log("User Left");
+        delete onlineUsers[socket.id];
+
+        io.emit("users-online", Object.values(onlineUsers));
+
+        console.log("User Left:", socket.id);
 
     });
 
 });
 
+
 server.listen(3000, () => {
 
-    console.log("Server Running");
+    console.log("Server Running on Port 3000");
 
 });
