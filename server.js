a7cfab512bc76f0ea3cac805286ed7abcb028ee5
ffshyw1-star <ffshyw1@gmail.com>
2026-07-26 23:35:50 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+
+    console.log("User Connected");
+
+    socket.on("chat-message", (data) => {
+
+        io.emit("chat-message", data);
+
+    });
+
+    socket.on("disconnect", () => {
+
+        console.log("User Left");
+
+    });
+
+});
+
+server.listen(3000, () => {
+
+    console.log("Server Running");
+
+});
